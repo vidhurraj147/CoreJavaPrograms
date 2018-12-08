03036ed02fac6faa9b780d950c6a0a3ffcb3de48
Lenovo <Lenovo@LAPTOP-KKISTNP1.lan1>
2018-12-08 13:14:36 -0500

    Core java examples programs

---
+++ CoreJavaPrograms/src/com/io/javabrains/UnitExercise1.java
@@ -0,0 +1,27 @@
+package com.io.javabrains;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class UnitExercise1 {
+
+    public static void main(String[] args) {
+        List<Person> people = Arrays.asList(
+                new Person("asdasd", "asdasd", 60),
+                new Person("asdas", "dfgdgf", 45),
+                new Person("asdasd", "gfdgdg", 77),
+                new Person("dfgdfg", "ujkythe", 32),
+                new Person("dfgd", "ethet", 23),
+                new Person("ryjjyj", "jyyjyj", 45),
+                new Person("veer", "", 44),
+                new Person("tryry", "rtyrty", 6456),
+                new Person("tetre", "sdvtnb", 65));
+
+        // Step1 : sort list by LastName
+
+        // Step2 : prints all elements in the list
+
+        // Step3 : LastName with letter d
+    }
+
+}
